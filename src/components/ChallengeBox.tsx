5a7358fd8907f82c42293ec708e4e9edbd61e040
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-02-25 20:16:42 -0300

    Aula 4

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,22 @@
 import { fail } from 'assert';
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
-
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext);
   // const hasActiveChallenge = true;
 
-  function failed() {
+  function handleChallengeSucceeded() {
+    completeChallenge();
+    resetCountdown();
+  }
 
+  function handleChallengeFailed() {
+    resetChallenge();
+    resetCountdown();
   }
 
   return (
@@ -26,14 +33,14 @@ export function ChallengeBox() {
             <button
               type="button"
               className={styles.challengeFailedButton}
-              onClick={resetChallenge}
+              onClick={handleChallengeFailed}
             >
               Falhei
             </button>
             <button
               type="button"
               className={styles.challengeSucceededButton}
-              onClick={failed}
+              onClick={handleChallengeSucceeded}
             >
               Completei
               </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,50 @@
 import Head from 'next/head';
+import { useContext, useEffect, useState } from 'react';
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompleteChallenges } from "../components/CompleteChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { NextLevel } from '../components/NextLevel';
 import { Profile } from "../components/Profile";
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
+  const { level } = useContext(ChallengesContext);
+  const [modalIsActive, setModalIsActive] = useState(false);
+
+  useEffect(() => {
+    if (level > 1) {
+      setModalIsActive(true);
+    }
+  }, [level]);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Inicio | Move.it</title>
-      </Head>
+    <>
+      <div className={styles.container} >
+        <Head>
+          <title>Inicio | Move.it</title>
+        </Head>
 
-      <ExperienceBar />
+        <ExperienceBar />
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompleteChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
 
-      <section>
-        <div>
-          <Profile />
-          <CompleteChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
-    </div>
+
+
+    </>
   )
 }
